650ae99c84f7eefe5cb93c5f9460e0dd95ac7ed3
SerjoGorbach <Kremi@ukr.net>
2013-05-17 11:20:21 +0300

    Create jin.h
    vtoroy .h

---
+++ jin.h
@@ -0,0 +1,23 @@
+
+#include "stdafx.h"
+#include <string>
+//#include <string>
+//#include <stdlib.h>
+using namespace std;
+class station
+{
+private:
+  string nameStation;
+	int numberPassengers;
+
+public:
+	//station();
+	void setstation(string ,int );
+	
+
+	int getName();
+
+	void displayName ();
+
+};
+
