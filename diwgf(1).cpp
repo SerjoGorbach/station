15894c612818db50e5f21e1684272b244b75f205
SerjoGorbach <Kremi@ukr.net>
2013-05-20 11:33:44 +0300

    Create diwgf(1).cpp

---
+++ diwgf(1).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "Gorbachidze.h"
+using namespace std;
+int main()
+{
+  TRAIN my;
+	string neStation;
+	int nberTrain;
+	int tiDeparture;
+	cout<<"name station : "<<endl;
+	cin>>neStation;
+	cout<<"number passengers  :"<<endl;
+	cin>>nberTrain;
+	cout<<"Time :"<<endl;
+	cin>>tiDeparture;
+	my.setTRAIN( neStation,nberTrain,tiDeparture);
+	my.displayName();
+	system("pause");
+	return 0;
+}
