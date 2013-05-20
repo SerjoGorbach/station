be5191c3fcfcd7d8a9ac88043c6dc040b9e73537
SerjoGorbach <Kremi@ukr.net>
2013-05-20 11:32:46 +0300

    Create Gorbachidze(1).h

---
+++ Gorbachidze(1).h
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <string>
+using namespace std;
+class TRAIN
+{
+private:
+string nameStation;
+int numberTrain;
+int timeDeparture;
+public:
+  //station();
+	void setTRAIN(string,int,int );
+
+
+	int getName();
+
+	void displayName ();
+
+};
